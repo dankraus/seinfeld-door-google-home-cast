217d4807d50661f0024b382b02c85293016c2458
Dan Kraus <dskraus@gmail.com>
2017-02-20 00:45:52 -0600

    Adds SmartThings app and doc

--- README.md
+++ README.md
@@ -1,13 +1,16 @@
-#Seinfeld Door Bass Riff Google Home Cast
+#Seinfeld Door Bass Riff SmartThings and Google Home Cast
 
-Casts a random Seinfeld transition bass riff or a HTTPS accessible MP3 through a Google Home device on your network.
+Casts a random Seinfeld transition bass riff or a HTTPS accessible MP3 through a Google Home device on your network. Can run on its own
+or triggered with a custom SmartThings app.
 
 ##Usage
+
+###Node
 Run `node app.js` first time and it will log the castable devices to your network. 
 The app will run at `http://localhost:3000` and also a publiclly available address via ngrok like `https://1234.ngrok.io`.
 
 Make an HTTP POST request to `/door` with a field called "ip" and the value of the IP address of the Google Home device on your network
-that was logged to the console. Ex:
+that was logged to the console. You don't need the port number. Ex:
 
 `curl -X POST -d "ip=192.168.1.215" https://1234.ngrok.io/door`
 
@@ -19,3 +22,11 @@ You can also play any MP3 from a URL with:
 `curl -X POST -d "ip=192.168.1.215&url=https://s3-us-west-2.amazonaws.com/blog-spokeo-test/seinfeld.mp3" https://1234.ngrok.io/url`
 
 Please note that the MP3 MUST be served over HTTPS.
+
+###SmartThings
+Set up an API account according to the SmartThings docs. Go to My SmartApps>New SmartApp. 
+Click the "From Code" tab, copy and paste the contents of `smartthings-app-seinfeld-door-entrance.groovy` and click 'Create'.
+From the SmartThings app, go to Automation>SmartApps>Add a SmartApp>My Apps and select Seinfeld Door Entrance. 
+Enter the ngrok address from the node app setup and the IP address of the Google Home you wish to cast too. Then select your door sensor.
+
+When the door opens, an HTTP POST request is made to the node app, the node app connects to the Google Home and casts an MP3 to it.
---
+++ smartthings-app-seinfeld-door-entrance.groovy
@@ -0,0 +1,70 @@
+/**
+ *  Seinfeld Door Entrance
+ *
+ *  Copyright 2017 Dan Kraus
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+definition(
+    name: "Seinfeld Door Entrance",
+    namespace: "dankraus",
+    author: "Dan Kraus",
+    description: "Calls the node app (dankraus/seinfeld-door-google-home-cast) when door switch is opened which plays a random Seinfeld bass riff on a Google Home device. Run the node app to get the ngrok address it is live at and the IP address of the Google Home to cast too",
+    category: "My Apps",
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
+
+
+preferences {
+    section("NodeJS app") {
+		input name: "nodeAppBaseAddress", type: "text", required: true, title: "NodeJS app available at (https://123abc.ngrok.io)"
+        input name: "googleHomeIP", type: "text", required: true, title: "IP Address of Google Home"
+	}
+    section("Sensor") {
+    	input name: "door", type: "capability.contactSensor", required: true, title: "Where?"
+    }
+}
+
+def installed() {
+	log.debug "Installed with settings: ${settings}"
+
+	initialize()
+}
+
+def updated() {
+	log.debug "Updated with settings: ${settings}"
+
+	unsubscribe()
+	initialize()
+}
+
+def initialize() {
+	subscribe(door, "contact.open", doorOpenHandler)
+}
+
+def doorOpenHandler(evt) {
+	log.info "Door Open Handler: ${evt}"
+    
+    try {
+    	def url = "$nodeAppBaseAddress/door"
+        def postParams = "ip=$googleHomeIP"
+
+        httpPost(
+	        uri: url,
+    	    body: [ip: googleHomeIP],
+        ) { 
+        	response -> log.debug "Played Seinfeld riff. Response data: ${resp.data}"
+        }
+    } catch (e) {
+    	log.debug "Call to NodeJS app failed: ${e}"
+    }
+}
